bfced42f75ec72044654f64ca610f18c2dc37a8c
Christoph Raupach <136818436+RaupeCHR@users.noreply.github.com>
2024-02-08 14:40:21 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from main import main 
+
+class TestMainFunction(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(main(), "Hello, World!")
+
+if __name__ == '__main__':
+    unittest.main()
