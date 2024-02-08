06d92e5652a05a60d65dc1a4058c365804483ac8
Christoph Raupach <136818436+RaupeCHR@users.noreply.github.com>
2024-02-08 14:41:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import main
 
 class TestMainFunction(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(main(), "Hello, World!")
+        self.assertEqual(main(), "Christoph")
 
 if __name__ == '__main__':
     unittest.main()
